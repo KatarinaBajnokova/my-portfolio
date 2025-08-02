0772c006dd15a0d6bf6ee775ea4ea8ec08c174b9
Katarina Bajnokova <r0997616@student.thomasmore.be>
2025-08-02 11:35:09 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,30 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
+  base: '/my-portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@styles': path.resolve(__dirname, './src/styles'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+       @use "@styles/base/colors" as *;
+        @use "@styles/base/typography" as *;
+        @use "@styles/base/mixins" as *;
+        @use "@styles/base/spacing" as *;
+        @use "@styles/utils/layout" as *;
+        @use "@styles/utils/structure" as *;
+        `,
+      },
     },
   },
   server: {
@@ -17,8 +32,7 @@ export default defineConfig({
     port: 5173,
   },
   build: {
-    outDir: "dist",
+    outDir: 'dist',
     sourcemap: true,
   },
-  base: "./",
 });
